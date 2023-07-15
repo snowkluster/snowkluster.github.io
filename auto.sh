898dcb377c1499d28f1d54d4210f6ae7811a3a07
Rohan Kaushik <rohankaushik1@outlook.com>
2023-07-15 17:13:10 +0530

    updated git auto script

--- auto.sh
+++ auto.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+Green='\033[0;32m'
 
 git add .
 
 echo -n "enter commit message: "
 
-read message
+read -r message
 
 git commit -S -m "$message"
 
 git push -u origin master
+
+echo "${Green}All Changes Pushed"
